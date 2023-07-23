17a1d7dbbb5556a9ee72d7754b415627954dd34f
M. Scott Ford <scott@corgibytes.com>
2023-07-22 22:46:32 -0400

    Switches to .NET 7

--- code/03-reducing-complexity/read-by-refactoring/commandline/src/CommandLine/Core/TokenPartitioner.cs
+++ code/03-reducing-complexity/read-by-refactoring/commandline/src/CommandLine/Core/TokenPartitioner.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using CommandLine.Infrastructure;
 using CSharpx;
+using ReferenceEqualityComparer = CommandLine.Infrastructure.ReferenceEqualityComparer;
 
 namespace CommandLine.Core
 {
