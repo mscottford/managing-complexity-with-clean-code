bbb0f6aafbae78f08f099674a718f848b31fc30d
M. Scott Ford <scott@corgibytes.com>
2023-07-24 15:46:46 -0400

    Adds examples for fighting polymorphism

---
+++ code/02-identifying-complexity/02-architectural-complexity/02-object-oriented-complexity-anti-patterns/02-fighting-polymorphism/HidingMembers/Program.cs
@@ -0,0 +1,19 @@
+﻿// This project can be run using `dotnet run`.
+
+var tiger = new Tiger();
+// Outputs "Speed as `Tiger`: 20"
+System.Console.WriteLine($"Speed as `Tiger`: {tiger.Speed}");
+
+var animal = tiger as Animal;
+// Outputs "Speed as `Animal`: 0"
+System.Console.WriteLine($"Speed as `Animal`: {animal.Speed}");
+
+abstract class Animal
+{
+    public virtual int Speed { get { return 0; } }
+}
+
+class Tiger : Animal
+{
+    public new int Speed { get { return 20; } }
+}
---
+++ code/02-identifying-complexity/02-architectural-complexity/02-object-oriented-complexity-anti-patterns/02-fighting-polymorphism/TypeTesting/Program.cs
@@ -0,0 +1,40 @@
+﻿// This project can be run using `dotnet run`.
+
+var tiger = new Tiger();
+var chicken = new Chicken();
+
+// Outputs "It's fast!"
+PrintSpeedStatus(tiger);
+// Outputs "It's slow!"
+PrintSpeedStatus(chicken);
+
+void PrintSpeedStatus(Animal animal)
+{
+    if (animal is Tiger)
+    {
+        System.Console.WriteLine("It's fast!");
+    }
+    else if (animal is Chicken)
+    {
+        System.Console.WriteLine("It's slow!");
+    }
+    else
+    {
+        System.Console.WriteLine("It's doesn't move!");
+    }
+}
+
+abstract class Animal
+{
+    public virtual int Speed { get { return 0; } }
+}
+
+class Tiger : Animal
+{
+    public new int Speed { get { return 20; } }
+}
+
+class Chicken : Animal
+{
+    public override int Speed { get { return 5; } }
+}
